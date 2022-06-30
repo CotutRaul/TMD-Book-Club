e2cd0e3e166d98ac750ad611f440a3c46ede6710
Cotut <raul.cotut@endava.com>
2022-06-30 18:03:31 +0300

    First Commit

---
+++ src/main/java/org/endava/tmd/TMDBookClub/TmdBookClubApplication.java
@@ -0,0 +1,15 @@
+package org.endava.tmd.TMDBookClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class TmdBookClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdBookClubApplication.class, args);
+	}
+
+}
