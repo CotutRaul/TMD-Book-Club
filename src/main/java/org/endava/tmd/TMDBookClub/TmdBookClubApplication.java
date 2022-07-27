de2a855bebfa4e172040f154c04c06c18cb1736a
Cotut <raul.cotut@endava.com>
2022-07-27 12:37:16 +0300

    Added Swagger

--- src/main/java/org/endava/tmd/TMDBookClub/TmdBookClubApplication.java
+++ src/main/java/org/endava/tmd/TMDBookClub/TmdBookClubApplication.java
@@ -1,9 +1,17 @@
 package org.endava.tmd.TMDBookClub;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "TMD Book Club API",
+				version = "1.0.0",
+				description = "An application to manage the books sharing"
+		))
 public class TmdBookClubApplication {
 
 	public static void main(String[] args) {
